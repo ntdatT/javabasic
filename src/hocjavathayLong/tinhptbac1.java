3d22321c346b3db5fb0610bf59d25b8b61ad4cb9
unknown <ntdat011193@gmail.com>
2023-09-02 11:56:18 +0700

    update pt bac 1

--- src/hocjavathayLong/tinhptbac1.java
+++ src/hocjavathayLong/tinhptbac1.java
@@ -4,21 +4,26 @@ import java.util.Scanner;
 
 public class tinhptbac1 {
     public static void main(String[] args) {
+        double a,b,x;
         Scanner sc = new Scanner(System.in);
         System.out.println("xin nhap a: ");
-        String a = sc.nextLine(); // hứng giá trị được nhập vào
+        a = sc.nextDouble(); // hứng giá trị được nhập vào
         System.out.println("a la:"+ a);
         System.out.println("xin nhap b: ");
-        String b = sc.nextLine(); // hứng giá trị được nhập vào
+        b = sc.nextDouble(); // hứng giá trị được nhập vào
         System.out.println("b la: "+ b);
-        System.out.println("xin nhap c: ");
-        String c = sc.nextLine(); // hứng giá trị được nhập vào
-        System.out.println("c la: "+ c);
-        if (Integer.parseInt(a) == 0){
-            System.out.println("a khong the bang 0");
-        }
-        else{
-            int x = (Integer.parseInt(c) - Integer.parseInt(b))/Integer.parseInt(a);
+
+        if (a == 0) {
+            //neu a = 0
+            if (b == 0) {
+                //va neu b =0
+                System.out.println("phuong trinh vo so nghiem");
+            } else {
+                // neu b khac 0
+                System.out.println("phuong trinh vo nghiem nha!!!!");
+            }
+        }else{
+            x = - b / a;
             System.out.println("x = " +x);
 
         }
